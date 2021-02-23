e15beb7d46e3f71cc7a531bc0dd41c1b874e9f4f
CocoBloom <virag.baranyai2@gmail.com>
2021-02-23 20:11:32 +0100

    create viewcounter migration

---
+++ framework-4.1.1/app/Database/Migrations/2021-02-23-183816_ViewCounter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class ViewCounter extends Migration
+{
+	public function up()
+	{
+        $this->db->disableForeignKeyChecks();
+
+        $this->forge->addField([
+            'photo_id' => [
+                'type'           => 'INT'
+            ],
+            'view_counter' => [
+                'type'           => 'INT',
+                'default'        => 0
+            ]
+        ]);
+
+        $this->forge->addForeignKey('photo_id', 'photos', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('view_counters', TRUE);
+
+        $this->db->enableForeignKeyChecks();
+
+    }
+
+	public function down()
+	{
+        $this->forge->dropTable('view_counters');
+
+    }
+}
