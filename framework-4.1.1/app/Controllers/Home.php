98f5a1268c9355e95599f074b6af4e9d2697d147
CocoBloom <virag.baranyai2@gmail.com>
2021-02-23 10:42:33 +0100

    create PhotoModel

--- framework-4.1.1/app/Controllers/Home.php
+++ framework-4.1.1/app/Controllers/Home.php
@@ -2,10 +2,17 @@
 
 namespace App\Controllers;
 
+use Config\Database;
+
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+
+//	    var_dump("conn<br/>");
+//        $db = Database::connect();
+//        echo "success";
+
+        return view('welcome_message');
 	}
 }
