7b2d7bddbc4c89fc13c57cf6f21c4f50931fd1e0
CocoBloom <virag.baranyai2@gmail.com>
2021-02-24 01:11:09 +0100

    implement PhotoController::index

--- framework-4.1.1/app/Controllers/Photo.php
+++ framework-4.1.1/app/Controllers/Photo.php
@@ -2,13 +2,17 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
+use Config\Database;
 
 class Photo extends BaseController
 {
 	public function index()
 	{
-		//
+        $builder = Database::connect()->table('photos');
+        $builder->select(['id', 'caption', 'photo_credit', 'view_counter']);
+        $builder->join('view_counters', 'photos.id = view_counters.photo_id');
+        $query = $builder->get();
+        return $query->getResultArray();
 	}
 
 
