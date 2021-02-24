b786e0eaabbd9461ce5f1af6c599db6499d3580e
CocoBloom <virag.baranyai2@gmail.com>
2021-02-24 10:19:10 +0100

    add the constructor of Photo Controller

--- framework-4.1.1/app/Controllers/Photo.php
+++ framework-4.1.1/app/Controllers/Photo.php
@@ -6,16 +6,21 @@ use Config\Database;
 
 class Photo extends BaseController
 {
-	public function index()
+    protected $db_connection;
+
+    public function __construct()
+    {
+        $this->db_connection = Database::connect();
+    }
+
+    public function index()
 	{
-        $builder = Database::connect()->table('photos');
+        $builder = $this->db_connection->table('photos');
         $builder->select(['id', 'caption', 'photo_credit', 'view_counter']);
         $builder->join('view_counters', 'photos.id = view_counters.photo_id');
         $query = $builder->get();
         return $query->getResultArray();
 	}
-
-
 
 
 }
