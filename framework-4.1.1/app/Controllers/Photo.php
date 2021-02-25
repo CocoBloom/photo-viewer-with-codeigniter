4ef6b4213c60984713686e7c7ca8f9e758373d15
CocoBloom <virag.baranyai2@gmail.com>
2021-02-25 03:06:23 +0100

    add create()

--- framework-4.1.1/app/Controllers/Photo.php
+++ framework-4.1.1/app/Controllers/Photo.php
@@ -3,20 +3,21 @@
 namespace App\Controllers;
 
 use CodeIgniter\HTTP\Request;
-use function PHPUnit\Framework\returnArgument;
 
 class Photo extends BaseController
 {
     protected $tableName = '';
+    protected $viewerTableName;
 
     public function __construct()
     {
         $this->tableName = 'photos';
+        $this->viewerTableName = 'view_counters';
     }
 
     public function index()
 	{
-        $builder = $this->db_connection->table('photos');
+        $builder = $this->db_connection->table($this->tableName);
         $builder->select(['id', 'caption', 'photo_credit', 'view_counter']);
         $builder->join('view_counters', 'photos.id = view_counters.photo_id');
         $query = $builder->get();
@@ -29,7 +30,7 @@ class Photo extends BaseController
 
 	public function show($id)
     {
-        $builder = $this->db_connection->table("photos");
+        $builder = $this->db_connection->table($this->tableName);
         $query = $builder->getWhere(['id' => $id]);
         if (!empty($query->getResult())) {
             return $this->response->setBody(["data" => $query->getResultArray()]);
@@ -40,14 +41,36 @@ class Photo extends BaseController
 
     public function delete($id)
     {
-        echo gettype($id)."\n".$this->tableName."\n";
         $builder = $this->db_connection->table($this->tableName);
         $builder->where('id', intval($id));
         $builder->delete();
-        if (0 === $this->db_connection->affectedRows()) {
-            var_dump($this->response->setBody(["data" => false]));
+        if (1 === $this->db_connection->affectedRows()) {
+            return $this->response->setBody(["data" => true]);
         } else {
-            var_dump($this->response->setBody(["data" => true]));
+            return $this->response->setBody(["data" => false]);
         }
+    }
+
+    public function create()
+    {
+        $data = $this->request->getPost();
+        $builder = $this->db_connection->table($this->tableName);
+        $builder->insert($data);
+        if ($this->db_connection->affectedRows() === 1 && $this->createPhotoViewerRow($this->db_connection->mysqli->insert_id)) {
+            return $this->response->setBody(["data" => true]);
+        } else {
+            return $this->response->setBody(["data" => false]);
+        }
+    }
+
+    public function createPhotoViewerRow($id): bool
+    {
+        $data = [
+            "photo_id" => $id,
+            "view_counter" => 0
+        ];
+        $builder = $this->db_connection->table($this->viewerTableName);
+        $builder->insert($data);
+        return $this->db_connection->affectedRows() === 1;
     }
 }
