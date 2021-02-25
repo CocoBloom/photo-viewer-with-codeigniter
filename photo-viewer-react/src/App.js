1453777e804b3254afef3d1f2e42fa949a2d955a
CocoBloom <virag.baranyai2@gmail.com>
2021-02-25 23:30:27 +0100

    add routing system into App.js

--- photo-viewer-react/src/App.js
+++ photo-viewer-react/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { MainPage } from './components/MainPage';
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    let content = (
+        <Router>
+            <div className="App">
+              <Route exact path="/" component={MainPage}></Route>
+      
+            </div>
+        </Router>
   );
+  return content;
 }
 
 export default App;
