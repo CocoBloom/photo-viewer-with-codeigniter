53e290a0a50dadd06da14ae05a6e3c1eddfb5a4d
CocoBloom <virag.baranyai2@gmail.com>
2021-02-27 12:17:27 +0100

    implement delete() and make cleaner index()

--- app/Controllers/PhotoController.php
+++ app/Controllers/PhotoController.php
@@ -9,10 +9,28 @@ class PhotoController extends ResourceController
     public function index()
     {
         $model = new Photo();
-        $photos = $model->select(['id', 'caption', 'photo_credit', 'view_counter'])
-            ->join('view_counters', 'photos.id = view_counters.photo_id')
-            ->findAll();
+        $photos = $model->join('view_counters', 'photos.id = view_counters.photo_id')
+                        ->findAll();
         return $this->respond($photos);
+    }
+
+    public function delete($id = null)
+    {
+        $model = new Photo();
+        $photoExists = $model->find($id);
+        if ($photoExists){
+            $model->delete($id);
+            $response = [
+                'status'   => 200,
+                'error'    => null,
+                'messages' => [
+                    'success' => 'Photo is deleted'
+                ]
+            ];
+            return $this->respondDeleted($response);
+        }else{
+            return $this->failNotFound('No Photo Found with this id: '.$id);
+        }
     }
 
 }
