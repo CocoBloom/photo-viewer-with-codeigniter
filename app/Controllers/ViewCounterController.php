12811edc05fe0035aed69b97119b652353e2fd3a
CocoBloom <virag.baranyai2@gmail.com>
2021-02-28 01:20:43 +0100

    implement update() in ViewCounterController.php

--- app/Controllers/ViewCounterController.php
+++ app/Controllers/ViewCounterController.php
@@ -5,7 +5,39 @@ namespace App\Controllers;
 use App\Models\ViewCounterModel;
 use CodeIgniter\RESTful\ResourceController;
 
-class PhotoController extends ResourceController
+class ViewCounterController extends ResourceController
 {
+    public function update($photo_id = null)
+    {
+        try {
+            $model = new ViewCounterModel();
+            $counterByPhotoID = $this->getCounterByPhotoID($photo_id);
+            $newCounter = [
+                'photo_id' => $photo_id,
+                'view_counter' => $counterByPhotoID + 1
+            ];
+
+            $model->protect(false)
+                ->save($newCounter);
+
+            $response = [
+                'status' => 200,
+                'error' => null,
+                'messages' => [
+                    'success' => 'Counter is increased.'
+                ]
+            ];
+            return $this->respondCreated($response);
+        } catch (\Exception $e) {
+            return $this->failNotFound($e->getMessage());
+        }
+    }
+
+    protected function getCounterByPhotoID($photo_id)
+    {
+        $model = new ViewCounterModel();
+        $view_counterByPhotoID = $model->getWhere(['photo_id' => $photo_id])->getResult();
+        return intval($view_counterByPhotoID[0]->view_counter);
+    }
 
 }
